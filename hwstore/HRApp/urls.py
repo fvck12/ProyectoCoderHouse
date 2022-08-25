85367d55616249293b536c7ca353e8f4c8ccb9e5
Omaration <omarive27@gmail.com>
2022-08-24 19:04:23 -0500

    Se agrega todo lo relacionado a Clientes

--- hwstore/HRApp/urls.py
+++ hwstore/HRApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from HRApp.views import ListarEmpleados, BusquedaEmpleado, CrearEmpleados, ActualizarEmpleados, BorrarEmpleados
+from HRApp.views import ListarClientes, ActualizarCliente, BorrarCliente, BusquedaCliente, CrearCliente
 from HRApp.views import hrAppInicio, login_request
 from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('', hrAppInicio, name="HRAppInicio"),
@@ -13,4 +15,10 @@ urlpatterns = [
     path('ActualizarEmpleados/<int:pk>', ActualizarEmpleados.as_view(), name="ActualizarEmpleados"),
     path('BorrarEmpleados/<int:pk>', BorrarEmpleados.as_view(), name="BorrarEmpleados"),
     path('BusquedaEmpleado/', BusquedaEmpleado.as_view(), name="BusquedaEmpleado"),
+
+    path('ListaClientes/', ListarClientes.as_view(), name="ListaClientes"),
+    path('CrearClientes/', CrearCliente.as_view(), name="CrearCliente"),
+    path('ActualizarClientes/<int:pk>', ActualizarCliente.as_view(), name="ActualizarCliente"),
+    path('BorrarClientes/<int:pk>', BorrarCliente.as_view(), name="BorrarCliente"),
+    path('BusquedaCliente/', BusquedaCliente.as_view(), name="BusquedaCliente"),
 ]
