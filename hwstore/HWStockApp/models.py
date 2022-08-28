33938b0db5c2b90ecb9128a4d5c61274b356c924
Sandwell <nicogonzasandoval@hotmail.com>
2022-08-28 14:09:38 -0300

    cambio de precio

--- hwstore/HWStockApp/models.py
+++ hwstore/HWStockApp/models.py
@@ -8,12 +8,7 @@ class Productos(models.Model):
     nombre = models.CharField(max_length=30)
     marca = models.CharField(max_length=20)
     stock = models.IntegerField()
-    precio = MoneyField(
-        decimal_places=2,
-        default=1,
-        default_currency='USD',
-        max_digits=11,
-    )
+    precio = models.IntegerField()
     Foto = models.ImageField(upload_to="Productos/", height_field=None,
                              width_field=None, max_length=None, null=True, blank=True)
 
