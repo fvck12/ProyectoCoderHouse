141329a292eaace03b766ecae24d590edecbd107
fvck12 <nico_martinez@hotmail.es>
2022-08-29 19:50:38 -0300

    Ultimos ajustes

--- hwstore/hwstore/urls.py
+++ hwstore/hwstore/urls.py
@@ -1,21 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from HWStoreApp import urls as store_url
-from HRApp import urls as hr_url
-from HWStockApp import urls as hwstock_url
-from hwstore.views import Register
 from django.conf import settings
 from django.conf.urls.static import static
 from HWStoreApp.views import HWStoreInicio
+
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('accounts/', include('Accounts.urls')),
-    path('', HWStoreInicio, name="HWStoreInicio"),
-    path('HWStoreApp/', include(store_url)),
-    path('HRApp/', include(hr_url)),
-    path('HWStockApp/', include(hwstock_url)),
-    #path('Registro/', Register, name="Registro"),    
+    path('', HWStoreInicio, name="HWStoreInicio"),    
+    path('HWStoreApp/', include('HWStoreApp.urls')),
+    path('HWStockApp/', include('HWStockApp.urls')),
+    path('accounts/', include('Accounts.urls')),   
 ]
 
 if settings.DEBUG:
